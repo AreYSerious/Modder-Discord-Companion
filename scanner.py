1ba950e05dadbb88cf173e8bcf593ce08a394bcd
AreYSerious <schroeder.niclas2001@gmail.com>
2023-04-15 16:08:09 +0200

    Try except main loop

--- scanner.py
+++ scanner.py
@@ -1,3 +1,5 @@
+import traceback
+
 from TOKEN import BOT_TOKEN
 import requests
 from bs4 import BeautifulSoup
@@ -40,149 +42,151 @@ async def on_ready():
 
 @tasks.loop(seconds=15)
 async def main_loop():
-    print("Starting Loop ...")
+    try:
 
-    base_url_for_link = "https://mods.vintagestory.at"
-    url = 'https://mods.vintagestory.at/home'
+        print("Starting Loop ...")
 
-    # Load the previous content from a pickle file if it exists
+        base_url_for_link = "https://mods.vintagestory.at"
+        url = 'https://mods.vintagestory.at/home'
 
-    data = {}
+        # Load the previous content from a pickle file if it exists
 
-    # Make a request to the website
-    response = requests.get(url)
+        data = {}
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+        # Make a request to the website
+        response = requests.get(url)
 
-    # Find the table on the webpage
-    table = soup.find('table')
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Find all rows in the table and skip the header row if present
-    rows = table.find_all('tr')[1:]
+        # Find the table on the webpage
+        table = soup.find('table')
 
-    messages = []
-    modauthors = []
+        # Find all rows in the table and skip the header row if present
+        rows = table.find_all('tr')[1:]
 
-    # Loop through the rows and extract the data
-    for row in rows:
-        # Find all columns in the row
-        columns = row.find_all('td')
+        messages = []
+        modauthors = []
 
-        # Extract the data from the columns
-        try:
+        # Loop through the rows and extract the data
+        for row in rows:
+            # Find all columns in the row
+            columns = row.find_all('td')
 
-            column_modname = columns[0].get_text()
+            # Extract the data from the columns
+            try:
 
-        except:
-            column_modname = "Couldnt find modname."
+                column_modname = columns[0].get_text()
 
-        try:
-            column_modurl = base_url_for_link + columns[2].find("a").get("href")
-        except:
-            column_modurl = "Couldnt find url."
+            except:
+                column_modname = "Couldnt find modname."
 
-        try:
-            #print(str(columns[1]))
-            column_text_unfinished = str(columns[1]).replace("<br/>", "\n").replace("<br>", "\n")
-            #print(column_text_unfinished)
-            column_text = BeautifulSoup(column_text_unfinished, 'html.parser').get_text()
-            #column_text = columns[1].get_text()
-        except:
-            column_text = "No text found."
+            try:
+                column_modurl = base_url_for_link + columns[2].find("a").get("href")
+            except:
+                column_modurl = "Couldnt find url."
 
-        try:
-            column_writer = columns[2].get_text()
-        except:
-            column_writer = "No writer found."
+            try:
+                #print(str(columns[1]))
+                column_text_unfinished = str(columns[1]).replace("<br/>", "\n").replace("<br>", "\n")
+                #print(column_text_unfinished)
+                column_text = BeautifulSoup(column_text_unfinished, 'html.parser').get_text()
+                #column_text = columns[1].get_text()
+            except:
+                column_text = "No text found."
 
-        try:
-            column_xago = columns[3].get_text()
-        except:
-            pass
+            try:
+                column_writer = columns[2].get_text()
+            except:
+                column_writer = "No writer found."
 
-        # Do whatever you need to do with the data
-        message = "Comment on " + column_modname + " from " + column_writer + ": " + column_text
-        messages.append(message)
+            try:
+                column_xago = columns[3].get_text()
+            except:
+                pass
 
-        modautorfinder_baseurl = "https://mods.vintagestory.at/api/mods?orderby=downloads&text="
-        modautorfinder_modname_reformat = str(column_modname).replace(" ", "%20")
-        modautorfinder_response = modautorfinder_baseurl + modautorfinder_modname_reformat
-        modautorfinder_data = requests.get(modautorfinder_response).json()
+            # Do whatever you need to do with the data
+            message = "Comment on " + column_modname + " from " + column_writer + ": " + column_text
+            messages.append(message)
 
-        # Extracting Data from JsonAPI
-        modautorfinder_author = modautorfinder_data["mods"][0]["author"]
-        modauthors.append(modautorfinder_author)
-        # print(modautorfinder_author)
-        # print("")
+            modautorfinder_baseurl = "https://mods.vintagestory.at/api/mods?orderby=downloads&text="
+            modautorfinder_modname_reformat = str(column_modname).replace(" ", "%20")
+            modautorfinder_response = modautorfinder_baseurl + modautorfinder_modname_reformat
+            modautorfinder_data = requests.get(modautorfinder_response).json()
 
-        data[message] = {"modname": column_modname, "modurl": column_modurl, "text": column_text,
-                         "writer": column_writer, "ago": column_xago, "messagereciever": modautorfinder_author}
-        # print(data)
+            # Extracting Data from JsonAPI
+            modautorfinder_author = modautorfinder_data["mods"][0]["author"]
+            modauthors.append(modautorfinder_author)
+            # print(modautorfinder_author)
+            # print("")
 
-    # new data = data
+            data[message] = {"modname": column_modname, "modurl": column_modurl, "text": column_text,
+                             "writer": column_writer, "ago": column_xago, "messagereciever": modautorfinder_author}
+            # print(data)
 
-    old_data = load_cache(f'data{os.sep}database.pickle')
+        # new data = data
 
-    for attribute, value in data.items():
-        if attribute in old_data:
-            print("Is known.")
-            pass
-        else:
-            print("Is new.")
+        old_data = load_cache(f'data{os.sep}database.pickle')
 
-            subscriber = load_cache(f'data{os.sep}data.pickle')
+        for attribute, value in data.items():
+            if attribute in old_data:
+                print("Is known.")
+                pass
+            else:
+                print("Is new.")
 
-            print("Loaded subscriber successfully.")
+                subscriber = load_cache(f'data{os.sep}data.pickle')
 
-            for xattribute, xvalue in subscriber.items():
+                print("Loaded subscriber successfully.")
 
-                if subscriber[xattribute] == value["messagereciever"]:
-                    print(f"Found a notification for {subscriber[xattribute]}")
-                    # if is not
-                    # This checks if the msg writer is the one getting notified and doesn't send a dm then
-                    if subscriber[xattribute] == value["writer"]:
-                        print(f"{subscriber[xattribute]} is {value['writer']}")
-                        pass
-                    else:
+                for xattribute, xvalue in subscriber.items():
+
+                    if subscriber[xattribute] == value["messagereciever"]:
+                        print(f"Found a notification for {subscriber[xattribute]}")
+                        # if is not
+                        # This checks if the msg writer is the one getting notified and doesn't send a dm then
+                        if subscriber[xattribute] == value["writer"]:
+                            print(f"{subscriber[xattribute]} is {value['writer']}")
+                            pass
+                        else:
+                            if len(value["text"]) >= 500:
+                                toshorten = value["text"]
+                                shortend = toshorten[:400]
+                                shortend = shortend + " ..."
+                                value["text"] = shortend
+                            print(value["text"].encode("utf-8"))
+                            user = await bot.fetch_user(xattribute)
+
+                            embed = discord.Embed(title="New comment on moddb!", url=value["modurl"], color=0xffffff)
+                            embed.set_thumbnail(url="https://mods.vintagestory.at/web/img/vsmoddb-logo-s.png")
+                            embed.add_field(name="Mod:", value=value["modname"], inline=False)
+                            embed.add_field(name="From:", value=value["writer"], inline=False)
+                            embed.add_field(name="Comment:", value=value["text"], inline=False)
+
+                            await user.send(embed=embed)
+
+                    if subscriber[xattribute] in value["text"]:
+                        #print(value["modname"])
+                        #print(value["text"])
+                        # if is not
+                        # This checks if the msg writer is the one getting notified and doesn't send a dm then
+
+
                         if len(value["text"]) >= 500:
-                            toshorten = value["text"]
-                            shortend = toshorten[:400]
-                            shortend = shortend + " ..."
-                            value["text"] = shortend
-                        print(value["text"].encode("utf-8"))
+                            secnd_toshorten = value["text"]
+                            secnd_shortend = secnd_toshorten[:400]
+                            secnd_shortend = secnd_shortend + " ..."
+                            value["text"] = secnd_shortend
+
                         user = await bot.fetch_user(xattribute)
 
-                        embed = discord.Embed(title="New comment on moddb!", url=value["modurl"], color=0xffffff)
-                        embed.set_thumbnail(url="https://mods.vintagestory.at/web/img/vsmoddb-logo-s.png")
-                        embed.add_field(name="Mod:", value=value["modname"], inline=False)
-                        embed.add_field(name="From:", value=value["writer"], inline=False)
-                        embed.add_field(name="Comment:", value=value["text"], inline=False)
+                        embed_ping = discord.Embed(title="New mention on moddb!", url=value["modurl"], color=0xffffff)
+                        embed_ping.set_thumbnail(url="https://mods.vintagestory.at/web/img/vsmoddb-logo-s.png")
+                        embed_ping.add_field(name="Mod:", value=value["modname"], inline=False)
+                        embed_ping.add_field(name="From:", value=value["writer"], inline=False)
+                        embed_ping.add_field(name="Comment:", value=value["text"], inline=False)
 
-                        await user.send(embed=embed)
-
-                if subscriber[xattribute] in value["text"]:
-                    #print(value["modname"])
-                    #print(value["text"])
-                    # if is not
-                    # This checks if the msg writer is the one getting notified and doesn't send a dm then
-
-
-                    if len(value["text"]) >= 500:
-                        secnd_toshorten = value["text"]
-                        secnd_shortend = secnd_toshorten[:400]
-                        secnd_shortend = secnd_shortend + " ..."
-                        value["text"] = secnd_shortend
-
-                    user = await bot.fetch_user(xattribute)
-
-                    embed_ping = discord.Embed(title="New mention on moddb!", url=value["modurl"], color=0xffffff)
-                    embed_ping.set_thumbnail(url="https://mods.vintagestory.at/web/img/vsmoddb-logo-s.png")
-                    embed_ping.add_field(name="Mod:", value=value["modname"], inline=False)
-                    embed_ping.add_field(name="From:", value=value["writer"], inline=False)
-                    embed_ping.add_field(name="Comment:", value=value["text"], inline=False)
-
-                    await user.send(embed=embed_ping)
+                        await user.send(embed=embed_ping)
 
 
 
@@ -193,11 +197,16 @@ async def main_loop():
 
 
 
-            old_data[attribute] = value
+                old_data[attribute] = value
 
-    save_cache(old_data, f'data{os.sep}database.pickle')
+        save_cache(old_data, f'data{os.sep}database.pickle')
 
-    print("... Successfully ending loop.")
+        print("... Successfully ending loop.")
+    except Exception:
+        print("Error in main loop! Following Error happened:")
+        traceback.print_exc()
+
+
 
 
 
